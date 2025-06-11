24f133eaf4c7de3cbce2d38ecc6987ce2ea8c625
lhk6666 <liuhaokun0039@hotmail.com>
2025-06-11 10:48:02 +0900

    [Models] Add slotmask_mlp_l

--- config/models.py
+++ config/models.py
@@ -1,12 +1,20 @@
 class ModelName:
-    def origin(self):
+    # trail_180
+    def origin_b(self):
         return "logs/dinov2_vitb14/version_0/checkpoints/epoch[19]_R@1[0.9311]_R@5[0.9581].ckpt"
     
-    def slot_mask(self):
+    # trial_181
+    def slot_mask_b(self):
         return "logs/dinov2_vitb14/version_8/checkpoints/epoch[16]_R@1[0.9324]_R@5[0.9608].ckpt"
     
-    def slot_mask_mlp(self):
+    # trial_175
+    def slot_mask_mlp_b(self):
         return "logs/dinov2_vitb14/version_17/checkpoints/epoch[15]_R@1[0.9311]_R@5[0.9622].ckpt"
     
-    def slot_mask_mlp_2(self):
+    # trial_177
+    def slot_mask_mlp_b_hiddenlayer(self):
         return "logs/dinov2_vitb14/version_25/checkpoints/epoch[21]_R@1[0.9338]_R@5[0.9622].ckpt"
+    
+    # trial_179
+    def slot_mask_mlp_l(self):
+        return "logs/dinov2_vitl14/version_1/checkpoints/epoch[34]_R@1[0.9311]_R@5[0.9635].ckpt"
